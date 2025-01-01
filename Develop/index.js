c4bc0aa059b2a47193a1dbe2e5c7c676dcf1aa59
Monty <slappytrap@gmail.com>
2024-12-31 20:08:07 -0800

    uploading to git

---
+++ Develop/index.js
@@ -0,0 +1,76 @@
+
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js';
+
+
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your application:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
+
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+    } else {
+      console.log(`${fileName} has been generated successfully!`);
+    }
+  });
+}
+
+
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const readmeContent = generateMarkdown(answers);
+    writeToFile('README.md', readmeContent);
+  });
+}
+
+
+init();
